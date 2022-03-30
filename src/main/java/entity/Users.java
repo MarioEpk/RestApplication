c3760acbfb69c640f8426c0893119a71c8f90cb2
Mario Alina <Epakfailer@gmail.com>
2022-03-30 23:24:52 +0200

    added username and password columns

--- src/main/java/entity/Users.java
+++ src/main/java/entity/Users.java
@@ -1,25 +1,22 @@
-package com.morosystems.restapplication.entity;
+package entity;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 
 @Entity
-@Table(name = "users", schema = "public", catalog = "MorosystemsUloha")
-public class UserEntity {
+public class Users {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
     @Column(name = "id")
     private int id;
     @Basic
     @Column(name = "name")
-    @NotNull
-    @Size(min = 2, message = "Name field should contain at least 2 characters.")
     private String name;
-
     @Basic
     @Column(name = "username")
     private String username;
+    @Basic
+    @Column(name = "password")
+    private String password;
 
     public int getId() {
         return id;
@@ -45,15 +42,25 @@ public class UserEntity {
         this.username = username;
     }
 
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
 
-        UserEntity that = (UserEntity) o;
+        Users users = (Users) o;
 
-        if (id != that.id) return false;
-        if (name != null ? !name.equals(that.name) : that.name != null) return false;
+        if (id != users.id) return false;
+        if (name != null ? !name.equals(users.name) : users.name != null) return false;
+        if (username != null ? !username.equals(users.username) : users.username != null) return false;
+        if (password != null ? !password.equals(users.password) : users.password != null) return false;
 
         return true;
     }
@@ -62,6 +69,8 @@ public class UserEntity {
     public int hashCode() {
         int result = id;
         result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (username != null ? username.hashCode() : 0);
+        result = 31 * result + (password != null ? password.hashCode() : 0);
         return result;
     }
 }
